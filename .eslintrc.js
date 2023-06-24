63994c6658539e8fb32ef3ef00ed856c1d1fff28
Christopher Villaran <cv.career.github.v0.pgs9v@simplelogin.com>
2023-06-24 21:39:50 +0800

    added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+/* eslint no-undef: "off" */
+module.exports = {
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
+};
