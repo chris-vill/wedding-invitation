7e68f7342f2dd292708a6a579fb0ec1842ac3b56
Christopher Villaran <cv.career.github.v0.pgs9v@simplelogin.com>
2023-06-26 16:15:36 +0800

    possible fix for nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ["styles/reset.sass", "styles/main.sass"],
+  css: ["@/styles/reset.sass", "@/styles/main.sass"],
   srcDir: "src/",
   typescript: {
     strict: true,
