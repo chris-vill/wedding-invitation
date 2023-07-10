1f2f19c0af2586577ca5ecdfa52efca9557afd08
Christopher Villaran <cv.career.github.v0.pgs9v@simplelogin.com>
2023-07-10 20:49:19 +0800

    ALSKJ

--- generate-secrets.js
+++ generate-secrets.js
@@ -15,4 +15,4 @@ const credentials = {
   client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
 };
 
-fs.writeFileSync("./dist/secrets.json", JSON.stringify(credentials, null, 2));
+fs.writeFileSync(".nuxt/secrets.json", JSON.stringify(credentials, null, 2));
