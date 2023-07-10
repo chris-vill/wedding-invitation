3edc7463e908a1326309df61a18a5b9a048352c2
Christopher Villaran <cv.career.github.v0.pgs9v@simplelogin.com>
2023-07-11 00:08:39 +0800

    ETO NA

--- generate-secrets.js
+++ generate-secrets.js
@@ -20,6 +20,6 @@ console.log(__dirname);
 // dist/secrets.json CORRECT
 
 fs.writeFileSync(
-  "/opt/build/repo/secrets.json",
+  "/opt/build/repo/dist/server/secrets.json",
   JSON.stringify(credentials, null, 2)
 );
