b9c7dd4ffe6660338bd436f5519fee4a55005175
Christopher Villaran <cv.career.github.v0.pgs9v@simplelogin.com>
2023-07-10 22:27:53 +0800

    asdasdasd

--- src/server/google-sheets.ts
+++ src/server/google-sheets.ts
@@ -1,8 +1,17 @@
 import { google } from "googleapis";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 async function getGoogleSheets() {
+  console.log("======================");
+  console.log(__dirname + process.env.SECRETS_PATH);
+  console.log("======================");
+
   const auth = await google.auth.getClient({
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    keyFile: __dirname + process.env.SECRETS_PATH,
   });
 
   return google.sheets({
